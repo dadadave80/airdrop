906bc6bfac3f3baaa1eee18d4c9936c06a61e0a0
David Dada <daveproxy80@gmail.com>
2025-09-30 23:02:17 +0100

    chore(fix): update Turbine prereq file name

--- scripts/generate-client.ts
+++ scripts/generate-client.ts
@@ -1,7 +1,7 @@
 import { createFromRoot } from "codama";
 import { rootNodeFromAnchor, type AnchorIdl } from "@codama/nodes-from-anchor";
 import { renderVisitor as renderJavaScriptVisitor } from "@codama/renderers-js";
-import anchorIdl from "../programs/Turbin3-prereq.json";
+import anchorIdl from "../programs/Turbin3_prereq.json";
 import path from "path";
 
 const codama = createFromRoot(rootNodeFromAnchor(anchorIdl as AnchorIdl));
